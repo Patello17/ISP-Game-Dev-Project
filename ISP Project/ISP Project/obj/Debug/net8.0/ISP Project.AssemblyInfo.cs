7f784f7fef15f81d720f6c3084f7aa56dfe007d6
24patel <118622262+Patello17@users.noreply.github.com>
2024-05-07 13:56:42 -0400

    added new Snail Mail Icon

--- ISP Project/ISP Project/obj/Debug/net8.0/ISP Project.AssemblyInfo.cs
+++ ISP Project/ISP Project/obj/Debug/net8.0/ISP Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ISP Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f569140a3ffa49b99b4416f4f1c0627e7eefd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+971801fd129b82b517eb1ecb63d181a9828cc269")]
 [assembly: System.Reflection.AssemblyProductAttribute("ISP Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ISP Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
